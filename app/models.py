501bd50f2daaf2a81116f377420fc6a575cccdbb
joaoPedro095810 <joaopedro095810@gmail.com>
2026-09-17 09:15:32 -0300

    feat: cria rota para atualizar livros

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from app.database import BaseBanco
 class Livro(BaseBanco):
     __tablename__ = "livros"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)  
     titulo = Column(String(150), nullable=False)
     autor = Column(String(120), nullable=False)
     ano_publicacao = Column(Integer, nullable=False)
